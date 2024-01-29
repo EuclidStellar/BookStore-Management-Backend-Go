2ca9f466839f4ed2fbfc128215f9033940eda30f
Euclid Stellar <euclidstellar@gmail.com>
2024-01-30 03:02:37 +0530

    V 1.1

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
-package main 
+package main
+
+import (
+	"bookstore/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RigesterBookStoreRooutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
